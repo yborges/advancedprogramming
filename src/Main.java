03d2ea7d317816ad6b004481d703f38a91da7a4c
Yuri Borges <yuriborges@me.com>
2018-10-03 15:36:05 +0200

    new main class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+package com.nasajens;
+import java.util.Scanner;
+import java.io.PrintStream;
+import java.util.Set;
+
+static final int MAX_NUMBER_OF_ELEMENTS = 10;
+        PrintStream out;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        start();
+    }
 }
+
+    boolean askSet (Scanner input, String question, Set set) {
+        do {
+            out.printf("%s", question);
+            if (! input.hasNextLine()) {
+                out.printf("\n"); // otherwise line with ^D will be overwritten
+                return false;
+            }
+        } while (! inputContainsCorrectSet(input, set));
+        return true;
+    }
+
+
+    boolean askBothSets (Scanner input, Set set1, Set set2) {
+        return askSet(input, "Give first set : ", set1) &&
+                askSet(input, "Give second set : ", set2);
+    }
+
+
+    static void start () {
+        Scanner in = new Scanner(System.in);
+        Set set1 = new Set(),
+                set2 = new Set();
+
+        while (askBothSets(in, set1, set2)) {
+            calculateAndGiveOutput(set1, set2);
+        }
+    }
+
+//TODO make inputContainsCorrectSet
+//Alphanumeric, max amount of elements, we still have to read the input into set.
+//TODO make calculateAndGiveOutput
+//make the four functions
+//TODO Yuri organize code
