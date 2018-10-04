11bcb594806a90a4de9b178a92aa931216469d6c
Jens van Groeningen <>
2018-10-04 12:09:34 +0200

    Werkende code tot vraag 'give first set:'

--- src/com/advancedprogramming/App.java
+++ src/com/advancedprogramming/App.java
@@ -24,38 +24,48 @@ import java.io.PrintStream;
 
 public class App {
 
-//    PrintStream out;
+    PrintStream out = new PrintStream(System.out);
+
     public static void main(String[] args) {
+        App app = new App();
+        app.start();;
+   }
 
-        Set set1 = new Set(), set2 = new Set();
+   void start() {
+       Set set1 = new Set(), set2 = new Set();
 
-        set1.add("JEns");
+       set1.add("JEns");
 
-        System.out.println(set1.get_size());
-        System.out.println(set2.get_size());
-//        Scanner in = new Scanner(System.in);
-//        Set set1 = new Set(), set2 = new Set();
-//
-//        while (askBothSets(in, set1, set2)) {
-//            calculateAndGiveOutput(set1, set2);
-//        }
+       System.out.println(set1.get_size());
+       System.out.println(set2.get_size());
+       Scanner in = new Scanner(System.in);
+
+
+       while (askBothSets(in, set1, set2)) {
+           //calculateAndGiveOutput(set1, set2);
+           out.print("inside while loop");//DEBUG
+       }
+   }
+
+    boolean askSet (Scanner input, String question, Set set) {
+        do {
+            out.printf("%s", question);
+            if (! input.hasNextLine()) {
+                out.printf("\n"); // otherwise line with ^D will be overwritten
+                return false;
+            }
+        } while (! inputContainsCorrectSet(input, set));
+        return true;
     }
 
-//    boolean askSet (Scanner input, String question, Set set) {
-//        do {
-//            out.printf("%s", question);
-//            if (! input.hasNextLine()) {
-//                out.printf("\n"); // otherwise line with ^D will be overwritten
-//                return false;
-//            }
-//        } while (! inputContainsCorrectSet(input, set));
-//        return true;
-//    }
-//
-//
-//    boolean askBothSets (Scanner input, Set set1, Set set2) {
-//        return askSet(input, "Give first set : ", set1) &&
-//                askSet(input, "Give second set : ", set2);
-//    }
+
+    boolean askBothSets (Scanner input, Set set1, Set set2) {
+        return askSet(input, "Give first set : ", set1) &&
+                askSet(input, "Give second set : ", set2);
+    }
+
+    boolean inputContainsCorrectSet(Scanner input, Set set) {
+        return true;
+    }
 
 }
